bcbb7d72eaba6f2d2798d7d9f5cd5a804d6506c7
Safar Ethan <ethansafar@gmail.com>
2022-07-11 18:36:53 +0200

    implementing lookUpTable in order to get rid of if/elseif/else forest

--- ex05/Harl.cpp
+++ ex05/Harl.cpp
@@ -10,3 +10,38 @@ Harl::~Harl( void ) {
 	return ;
 }
 
+void	Harl::complain( std::string level ) {
+	
+	const	char * const lookUpTable[5] = {
+		debug();
+		info();
+		warning();
+		error();
+	};
+
+	return ;
+}
+
+void	Harl::debug( void ) {
+
+	std::cout << "Debug" << std::endl;
+	return ;
+}
+
+void	Harl::info( void ) {
+
+	std::cout << "Info" << std::endl;
+	return ;
+}
+
+void	Harl::warning( void ) {
+
+	std::cout << "Warning" << std::endl;
+	return ;
+}
+
+void	Harl::error( void ) {
+
+	std::cout << "Error" << std::endl;
+	return ;
+}
