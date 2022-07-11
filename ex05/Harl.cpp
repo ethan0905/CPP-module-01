b3607ccb6b21c62bcf9fdad2b26141e9768bb1c2
Safar Ethan <ethansafar@gmail.com>
2022-07-11 19:03:30 +0200

    working program, cleaned code, added error handling + main easier to use

--- ex05/Harl.cpp
+++ ex05/Harl.cpp
@@ -1,47 +1,64 @@
 #include "Harl.hpp"
 
 Harl::Harl( void ) {
-
+	
+	std::cout << "Constructor has been called." << std::endl;
 	return ;
 }
 
 Harl::~Harl( void ) {
 
+	std::cout << "Destructor has been called." << std::endl;
 	return ;
 }
 
 void	Harl::complain( std::string level ) {
 	
-	const	char * const lookUpTable[5] = {
-		debug();
-		info();
-		warning();
-		error();
+	void (Harl::*lookUpTable[4])(void) = {
+		&Harl::debug,
+		&Harl::info,
+		&Harl::warning,
+		&Harl::error
 	};
 
+	std::string	levels[4] = {
+		"DEBUG",
+		"INFO",
+		"WARNING",
+		"ERROR"
+	};
+
+	for (int i = 0; i < 4; i++)
+	{
+		if (levels[i] == level)
+		{
+			(this->*lookUpTable[i])();
+			break ;
+		}
+	}
 	return ;
 }
 
 void	Harl::debug( void ) {
 
-	std::cout << "Debug" << std::endl;
+	std::cout << "[DEBUG] function has been called." << std::endl;
 	return ;
 }
 
 void	Harl::info( void ) {
 
-	std::cout << "Info" << std::endl;
+	std::cout << "[INFO] function has been called." << std::endl;
 	return ;
 }
 
 void	Harl::warning( void ) {
 
-	std::cout << "Warning" << std::endl;
+	std::cout << "[WARNING] function has been called." << std::endl;
 	return ;
 }
 
 void	Harl::error( void ) {
 
-	std::cout << "Error" << std::endl;
+	std::cout << "[ERROR] function has been called." << std::endl;
 	return ;
 }
