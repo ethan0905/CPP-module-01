0d6208f388eae6f101f862b24d8a8d2a2e28d88c
Safar Ethan <ethansafar@gmail.com>
2022-07-11 17:30:49 +0200

    worked on error management + infile/outfile creation

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -3,15 +3,34 @@
 
 void	replace( std::string filename, std::string s1, std::string s2 )
 {
-	std::ifstream	infile;
+	(void)s1;
+	(void)s2;
+	std::ifstream	infile(filename);
 
-	infile.open(av[1]);
+	if (!infile)
+	{
+		std::cerr << "Error: " << filename << ": invalid infile." << std::endl;
+		return ;
+	}
+	if (s1.empty())
+	{
+		std::cerr << "Error: s1: invalid string." << std::endl;
+		return ;
+	}
 
+	std::ofstream	outfile(filename + ".res");
+	
+	if (!outfile)
+	{
+		std::cerr << "Error: outfile couldn't be created." << std::endl;
+		return ;
+	}
 	std::string	line;
 	while (std::getline(infile, line))
 		std::cout << line << std::endl;
 
 	infile.close();
+	outfile.close();
 }
 
 int	main(int ac, char **av)
